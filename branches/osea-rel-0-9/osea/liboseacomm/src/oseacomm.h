9078ce0eea952c3b691d45649fb936272f6ae555
davefx <davefx@c3816bf4-cfd4-0310-97f6-f0d05edf9206>
2004-07-27 17:26:25 +0000

    Adding new version of oseacomm.h, with the new future interface. Cleaner, and more powerful (I hope!)

---
+++ branches/osea-rel-0-9/osea/liboseacomm/src/oseacomm.h
@@ -0,0 +1,64 @@
+//  LibOseaComm:  Support library with xml and communication functions.
+//  Copyright (C) 2002, 2003 Advanced Software Production Line, S.L.
+//  Copyright (C) 2004 David Marín Carreño
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or   
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#ifndef __OSEACOMM_H__
+#define __OSEACOMM_H__
+#include <glib.h>
+
+
+gboolean oseacomm_init                      (gint *argc, gchar ***argv, GError **error);
+
+gboolean oseacomm_set_properties            (gchar *property_name,
+					     gpointer value, ...);
+
+gboolean oseacomm_connect                   (gchar *peer_name);
+gboolean oseacomm_disconnect                (gchar *peer_name);
+
+gboolean oseacomm_set_connection_properties (gchar *peer_name,
+					     gchar *property_name,
+					     gpointer value, ...);
+
+
+gint     oseacomm_send                      (gchar *peer_name,
+			                     OseaCommPartialMessage *message,
+			                     gboolean is_last);
+
+typedef  gboolean (*OseaCommReplyReceive)   (gint message_id,
+					     gint error_code,
+					     OseaCommPartialMessage *message,
+					     gboolean is_last);
+
+
+gboolean oseacomm_set_listener_properties   (gchar *property_name,
+					     gpointer value, ...);
+
+gboolean oseacomm_wait                      (gint port);
+
+typedef  gboolean (*OseaCommMessageReceive) (gint message_id,
+					     OseaCommPeerInfo *peer_info,
+					     OseaCommPartialMessage *message,
+					     gboolean is_last);
+
+gboolean oseacomm_reply                     (gint message_id,
+					     gint error_code,
+					     OseaCommPartialMessage *message,
+					     gboolean is_last);
+
+gboolean oseacomm_exit (GError **error);
+
+#endif
