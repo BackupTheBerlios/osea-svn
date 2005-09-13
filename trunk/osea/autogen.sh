7fba405f4168090f447207c1a2c9ab5723ba6137
davefx <davefx@c3816bf4-cfd4-0310-97f6-f0d05edf9206>
2005-09-13 17:50:12 +0000

    Eliminando libshieldvortex: no nos basaremos finalmente en vortex

--- trunk/osea/autogen.sh
+++ trunk/osea/autogen.sh
@@ -4,19 +4,11 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="GNOME Data Access"
+PKG_NAME="öSea system"
 AUTOCONF=autoconf
 REQUIRED_AUTOCONF_VERSION=2.59
-AUTOMAKE=automake-1.9
-REQUIRED_AUTOMAKE_VERSION=1.9
-
-(test -f $srcdir/configure.in \
-  && test -d $srcdir/idl \
-  && test -f $srcdir/idl/GNOME_Database.idl) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level GDA directory"
-    exit 1
-}
+AUTOMAKE=automake-1.6
+REQUIRED_AUTOMAKE_VERSION=1.6
 
 gnome_autogen=`which gnome-autogen.sh`
 test -z "$gnome_autogen"
